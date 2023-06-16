3ad9dfbbbbf20e299e1b471e46605735f7f2ab2a
jorge <jorge@nursa.com>
2023-06-16 14:49:58 -0300

    testing code

--- sliding-box.js
+++ sliding-box.js
@@ -10,12 +10,74 @@ let newDomObject = document.querySelector('.box');
 
 const vw = window.innerWidth;
 const vh = window.innerHeight;
+const velocity = 10;
+const boxHeight = 50;
+const boxWidth = 50;
 
 let x = Math.floor(vw / 2); // x starts half of width
 let y = Math.floor(vh / 2); // y starts half of height
 
-const animate = ()=>{
-    if(x <= window.AAAAAAAAAAAAAAAAAAA) {
+let isAnimating = false;
+let animationInterval = 0;
 
+document.body.addEventListener('keydown', (event) => {
+    move(event.key)
+})
+
+function move(direction) {
+    switch (direction) {
+        case 'ArrowLeft':
+            x -= velocity;
+            break;
+        case 'ArrowRight':
+            x += velocity
+            break;
+        case 'ArrowUp':
+            y -= velocity
+            break;
+        case 'ArrowDown':
+            y += velocity;
+            break;
+    }
+
+    renderBox(x, y)
+}
+
+function toggleAnimation() {
+    console.log('animation')
+    if (isAnimating) {
+        stopAnimation();
+    } else {
+        startAnimation();
     }
 }
+
+function startAnimation() {
+    animationInterval = setInterval(() => {
+        animate();
+    }, 100);
+    isAnimating = true;
+}
+
+function stopAnimation() {
+    clearInterval(animationInterval);
+    isAnimating = false;
+}
+
+function renderBox(left, top) {
+    if(x + boxWidth > window.innerWidth) {
+        x = 0;
+    }
+    if(y + boxHeight > window.innerHeight) {
+        y = 0;
+    }
+    newDomObject.setAttribute('style', `margin-left: ${left}px; margin-top: ${top}px; `)
+}
+
+const animate = ()=> {
+    x += velocity;
+    y += velocity;
+
+    renderBox(x, y)
+}
+
