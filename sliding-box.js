5c89875de19202cfb64a36cf68c2d12e09868a92
CNichol-Prime <129085193+CNichol-Prime@users.noreply.github.com>
2023-06-16 12:49:56 -0400

    starting upload
    uploaded directly to github

---
+++ sliding-box.js
@@ -0,0 +1,21 @@
+let domObject = document.createElement('div');
+
+domObject.setAttribute('style', '');
+
+document.body.appendChild(domObject);
+
+domObject.classList.add('box');
+
+let newDomObject = document.querySelector('.box');
+
+const vw = window.innerWidth;
+const vh = window.innerHeight;
+
+let x = Math.floor(vw / 2); // x starts half of width
+let y = Math.floor(vh / 2); // y starts half of height
+
+const animate = ()=>{
+    if(x <= window.AAAAAAAAAAAAAAAAAAA) {
+
+    }
+}
